791b8044cff055743ef3166f35b5705a76c2e982
Ciprian <botnari_ciprian@mail.ru>
2024-06-15 20:23:56 +0300

    feat: update advantages

--- frontend/react-app/src/components/pages/Home.jsx
+++ frontend/react-app/src/components/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "../Header";
 import ChoicePrompt from "../ChoicePrompt";
 
@@ -9,20 +8,17 @@ function Home() {
       <ChoicePrompt
         optionA="Mercedes Maybach"
         optionB="Toyota Camry"
-        infoA={`
-        Offers unparalleled luxury and performance.
-
-        It is fast and packed with high-end features.
-
-        Provides an exceptional driving experience.
-
-        But it comes with a very high price tag and expensive maintenance costs.
-        `.trim()}
-        infoB={`
-        It is a reliable and affordable option with great fuel efficiency and a comfortable ride.
-        It is known for its durability and low cost of ownership.
-        But it lacks the luxurious features and performance of high-end cars like the Maybach.
-        `.trim()}
+        infoA={[
+          "Offers unparalleled luxury and performance.",
+          "It is fast and packed with high-end features.",
+          "Provides an exceptional driving experience.",
+          "But it comes with a very high price tag and expensive maintenance costs.",
+        ]}
+        infoB={[
+          "It is a reliable and affordable option with great fuel efficiency and a comfortable ride.",
+          "It is known for its durability and low cost of ownership.",
+          "But it lacks the luxurious features and performance of high-end cars like the Maybach.",
+        ]}
       />
     </>
   );
