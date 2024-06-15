2ed08cbf9ce87b3354d49e2a10652d389da5691a
buffaloss <beatricia.golban@isa.utm.md>
2024-06-15 19:14:23 +0300

    feat: add choice prompt

--- frontend/react-app/src/components/pages/Home.jsx
+++ frontend/react-app/src/components/pages/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import Header
- from '../Header';
+import React from "react";
+import Header from "../Header";
+import ChoicePrompt from "../ChoicePrompt";
+
 function Home() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <ChoicePrompt
+        optionA="Mercedes Maybach"
+        optionB="Toyota Camry"
+        infoA="The Mercedes Maybach offers unparalleled luxury and performance. It is fast and packed with high-end features, providing an exceptional driving experience. BUT it comes with a very high price tag and expensive maintenance costs."
+        infoB="The Toyota Camry is a reliable and affordable option with great fuel efficiency and a comfortable ride. It is known for its durability and low cost of ownership. BUT it lacks the luxurious features and performance of high-end cars like the Maybach."
+      />
+    </>
   );
 }
 
