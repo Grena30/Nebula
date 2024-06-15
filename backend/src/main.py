f3debd95f4e4060c44dd424ef1f0f7173f5d0989
Sorin <89846259+Syn4z@users.noreply.github.com>
2024-06-15 22:42:08 +0300

    Fix/parser (#9)
    * feat: add car image folder
    
    * feat: add mercedes images
    
    * feat: add bmw images
    
    * fix: change parser; store llm results
    
    ---------
    
    Co-authored-by: Grena30 <grenadinu@yahoo.com>

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,11 @@
 import cohere 
 import dotenv
+from response_parser import parse_response
 
 config = dotenv.dotenv_values()
 
 co = cohere.Client(
-  api_key=config['API_KEY'], # This is your trial API key
+  api_key=config['API_KEY'],
 ) 
 
 # Prompt structure: 
@@ -21,17 +22,17 @@ def llm_prompt(car_filters, additional=None):
 
   prompt_message += "Give me the details for each car following this structure:\n"
   # prompt_message += "Full Name, Price (only in american dollars),  Manufacturing year, Drive system, Transmission, Perfomance (acceleration 0-100), Range (maximum range), Additional info.\n"
-  prompt_message += "<brand> Insert brand name here</brand>"
-  prompt_message += "<model> Insert model name here</model>"
-  prompt_message += "<body type> Insert body type here</body type>"
+  prompt_message += "<brand> Insert brand name here</brand>\n"
+  prompt_message += "<model> Insert model name here</model>\n"
+  prompt_message += "<body type> Insert body type here</body type>\n"
   prompt_message += "<manufacturing year> Insert manufacturing year here</manufacturing year>\n"
-  prompt_message += "<class> Insert class here</class>"
-  prompt_message += "<price> Insert here price </price>"
+  prompt_message += "<class> Insert class here</class>\n"
+  prompt_message += "<price> Insert here price </price>\n"
   prompt_message += "<powertrain> Insert powertrain here</powertrain>\n"
-  prompt_message += "<drive system> Insert drive system here</drive system>\n"
-  prompt_message += "<transmission type> Insert transmission type here</transmissison type>\n"
-  prompt_message += "<perfomance> Insert performance from 0-100 in seconds here</perfomanec>\n"
-  prompt_message += "<range> Insert range for consumption here</range>\n"
+  # prompt_message += "<drive system> Insert drive system here</drive system>\n"
+  # prompt_message += "<transmission type> Insert transmission type here</transmissison type>\n"
+  # prompt_message += "<perfomance> Insert performance from 0-100 in seconds here</perfomanec>\n"
+  # prompt_message += "<range> Insert range for consumption here</range>\n"
   prompt_message += "<additional> Insert any additional info here</additional>\n"
   # prompt_message += "Important if you cannot specify exactly one category then approximate or give a range of values instead of not available. If you have any additional info for each car add the rest of the info to the additional part if not then leave empty\n"
   prompt_message += "Important! look up cars manufacturer website, or official dealer data for the most accurate information, search different kind of sources in order to find all the information\n"
@@ -55,11 +56,15 @@ def llm_prompt(car_filters, additional=None):
     connectors=[{"id":"web-search"}]
   ) 
 
+  result = ""
   for event in stream:
     if event.event_type == "text-generation":
-      print(event.text, end='')
+      result += event.text
+  return result    
 
 
-car_filters = {"Brand": "Mercedes"}
+car_filters = {"Brand": "Mercedes", "Body Type": "SUV"}
 # additional = "Suitable for 2 kids"
-llm_prompt(car_filters)
+data=llm_prompt(car_filters)
+parsed_data=parse_response(data)
+print(parsed_data)
--- backend/src/response_parser.py
+++ backend/src/response_parser.py
@@ -3,30 +3,19 @@ import re
 
 
 def parse_response(response):
-    parsed_response = {}
+    pattern = re.compile(r"<(.*?)>(.*?)</\1>", re.DOTALL)
+    matches = pattern.findall(response)
+    car_list = []
+    current_car = {}
 
-    car_key = None
-    for row in response.split("\n"):
-        if row.startswith("##"):
-            row = row.replace("<brand>", "").replace("</brand>", "").strip()
-            key = row.replace("##", "").strip()
-            parsed_response[key] = {}
-            car_key = key
-        elif row.startswith("<"):
-            spec_key = row.split("<")[1].split(">")[0]
-            spec_value = row.split(">")[1].split("<")[0].strip()
+    for tag, value in matches:
+        tag = tag.strip()
+        value = value.strip()
+        if tag == 'brand' and current_car:
+            car_list.append(current_car)
+            current_car = {}
+        current_car[tag] = value
 
-            if spec_key == "price" and spec_value != "N/A" and spec_value != "Not available":
-                currency_symbol = re.search(r'[^\d.,]', spec_value).group()
-                currency_amount = re.sub(r'[^\d.]', '', spec_value)
-
-                if currency_symbol == "$":
-                    currency_symbol = "USD"
-                else:
-                    currency_symbol = iso4217parse.parse(currency_symbol)[0].alpha3
-
-                spec_value = currency_symbol + " " + currency_amount
-
-            parsed_response[car_key][spec_key] = spec_value
-
-    return parsed_response
+    if current_car:
+        car_list.append(current_car)
+    return car_list
