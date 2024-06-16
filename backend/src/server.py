a122221b7738be2aaac1b735489e7070ad2c3418
Ciprian <botnari_ciprian@mail.ru>
2024-06-16 10:18:18 +0300

    feat: add results

--- backend/src/server.py
+++ backend/src/server.py
@@ -2,20 +2,36 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from src.main import llm_prompt
 from src.response_parser import parse_response
+import json
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
+
+# @app.route('/api/search', methods=['POST'])
+# def search():
+#     data = request.get_json()
+#     response: dict = None
+#     if "additional" in data.keys():
+#         additional = data["additional"]
+#         data.pop("additional")
+#         response = llm_prompt(data, additional)
+#     else:
+#         response = llm_prompt(data)
+
+#     return jsonify(parse_response(response))
 
 @app.route('/api/search', methods=['POST'])
 def search():
-    data = request.get_json()
-    response: dict = None
-    if "additional" in data.keys():
-        additional = data["additional"]
-        data.pop("additional")
-        response = llm_prompt(data, additional)
-    else:
-        response = llm_prompt(data)
-
-    return jsonify(parse_response(response))
+    # data = request.get_json()
+    # response: dict = None
+    # if "additional" in data.keys():
+    #     additional = data["additional"]
+    #     data.pop("additional")
+    #     response = llm_prompt(data, additional)
+    # else:
+        # response = llm_prompt(data)
+    with open ("example1.json", "r") as file:
+        data = json.load(file)
+    
+    return jsonify(data)
--- frontend/react-app/src/components/pages/Shop.jsx
+++ frontend/react-app/src/components/pages/Shop.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import "../../css/Shop.css";
+import CarList from "../CarList";
 
 const Shop = () => {
+  const [cars, setCars] = useState([]); 
   const brands = [
     "BMW",
     "Mercedes",
@@ -21,7 +23,7 @@ const Shop = () => {
     "https://www.topgear.com/sites/default/files/news-listicle/image/2023/09/LEAD.jpg",
     "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/ijsxlvQB.bu8/v0/-1x-1.jpg",
     "https://media.cdn-jaguarlandrover.com/api/v2/images/101871/w/1600.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/8/8a/2020_Volkswagen_Golf_Style_1.5_Front.jpg"
+    "https://upload.wikimedia.org/wikipedia/commons/8/8a/2020_Volkswagen_Golf_Style_1.5_Front.jpg",
   ];
   const [activeCarClasses, setActiveCarClasses] = useState([]);
 
@@ -30,11 +32,12 @@ const Shop = () => {
     "https://images.topgear.com.ph/topgear/images/2020/08/07/camnry-black-edition-1-1596789185.jpg",
     "https://www.complete-leasing.co.uk/img/gallery/lexus-rc-coupe-car.jpg",
     "https://www.actualidadmotor.com/wp-content/uploads/2023/09/toyota-yaris-130h-2-768x461.jpg",
-    "https://media.ed.edmunds-media.com/mitsubishi/outlander-sport/2024/oem/2024_mitsubishi_outlander-sport_4dr-suv_20-se_fq_oem_1_1600.jpg"
+    "https://media.ed.edmunds-media.com/mitsubishi/outlander-sport/2024/oem/2024_mitsubishi_outlander-sport_4dr-suv_20-se_fq_oem_1_1600.jpg",
   ];
   const [activeBodyTypes, setActiveBodyTypes] = useState([]);
 
-  const [additional, setAdditional] = useState('');
+  const [additional, setAdditional] = useState("");
+  const apiUrl = "http://127.0.0.1:5000/api/search";
 
   const handleClick = (item, items, setActiveItems, activeItems) => {
     if (item === "All") {
@@ -55,7 +58,25 @@ const Shop = () => {
   };
 
   const handleSubmit = () => {
-    
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        brands: activeBrands.join(),
+        powertrains: activePowertrains.join(),
+        carClasses: activeCarClasses.join(),
+        bodyTypes: activeBodyTypes.join(),
+        additional: additional,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCars(data);
+      })
+      .catch((error) => console.log(error));
   };
 
   return (
@@ -184,12 +205,7 @@ const Shop = () => {
               activeBodyTypes.length === bodyTypes.length ? "active" : ""
             }`}
             onClick={() =>
-              handleClick(
-                "All",
-                bodyTypes,
-                setActiveBodyTypes,
-                activeBodyTypes
-              )
+              handleClick("All", bodyTypes, setActiveBodyTypes, activeBodyTypes)
             }
           >
             All
@@ -235,8 +251,18 @@ const Shop = () => {
       </div>
 
       <div className="form-group">
-        <button className="btn btn-submit" onClick={handleSubmit}>Submit</button>
+        <button className="btn btn-submit" onClick={handleSubmit}>
+          Search
+        </button>
       </div>
+
+      {
+        cars.length > 0 &&
+        <div className="form-group">
+          <h2 className="form-group-title">Results</h2>
+          <CarList carsResults={cars} />
+        </div>
+      }
     </div>
   );
 };
