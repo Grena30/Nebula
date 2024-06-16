5ebb8e051929075eda30ed21a1ab6b3823fb1e54
Ciprian <botnari_ciprian@mail.ru>
2024-06-16 10:54:09 +0300

    feat: move images to public folder

--- frontend/react-app/src/components/Car.jsx
+++ frontend/react-app/src/components/Car.jsx
@@ -1,18 +1,31 @@
-import '../css/Car.css'
+import "../css/Car.css";
 
 const Car = ({ car }) => {
   return (
     <div className="car-card">
-      <img className="car-img" src={car.imageUrl} alt={car.make} />
-      <h2 className="car-title">{car.brand} {car.model}</h2>
-      <p className="car-detail">Body type: {car["body type"]}</p>
-      <p className="car-detail">Year: {car["manufacturing year"]}</p>
-      <p className="car-detail">Class: {car.class}</p>
-      <p className="car-detail">Price: {car.price}</p>
-      <p className="car-detail">Powertrain: {car.powertrain}</p>
-      <p className="car-detail car-detail-text">Additional: {car.additional}</p>
+      <img className="car-card-img" src={car.imageUrl} alt={car.make} />
+      <h2 className="car-title">
+        {car.brand} {car.model}
+      </h2>
+      <p className="car-detail">
+        <span className="detail-highlight">Body type:</span> {car["body type"]}
+      </p>
+      <p className="car-detail">
+        <span className="detail-highlight">Year: </span>
+        {car["manufacturing year"]}
+      </p>
+      <p className="car-detail">
+        <span className="detail-highlight">Color: </span>   
+        {car.class}</p>
+      <p className="car-detail">
+        <span className="detail-highlight">Price: </span>
+        {car.price}</p>
+      <p className="car-detail">
+        <span className="detail-highlight">Powertrain: </span>
+        {car.powertrain}</p>
+      <p className="car-detail car-detail-text">{car.additional}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Car
+export default Car;
