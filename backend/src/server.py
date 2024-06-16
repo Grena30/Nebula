6fdddd2dd54c5dff05c653145c993ef0c6566a65
Sorin <89846259+Syn4z@users.noreply.github.com>
2024-06-16 10:17:28 +0300

    fix: update images (#14)

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from src.main import llm_prompt
@@ -7,15 +8,30 @@ from src.response_parser import parse_response
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
+# @app.route('/api/search', methods=['POST'])
+# def search():
+#     data = request.get_json()
+#     response: dict = None
+#     if "additional" in data.keys():
+#         additional = data["additional"]
+#         data.pop("additional")
+#         response = llm_prompt(data, additional)
+#     else:
+#         response = llm_prompt(data)
+#     with open ("example1.json", "r") as file:
+#         data = json.load(file)
+#     return jsonify(parse_response(data))
+
 @app.route('/api/search', methods=['POST'])
 def search():
-    data = request.get_json()
-    response: dict = None
-    if "additional" in data.keys():
-        additional = data["additional"]
-        data.pop("additional")
-        response = llm_prompt(data, additional)
-    else:
-        response = llm_prompt(data)
-
-    return jsonify(parse_response(response))
+    # data = request.get_json()
+    # response: dict = None
+    # if "additional" in data.keys():
+    #     additional = data["additional"]
+    #     data.pop("additional")
+    #     response = llm_prompt(data, additional)
+    # else:
+    #     response = llm_prompt(data)
+    with open ("src/example1.json", "r") as file:
+        data = json.load(file)
+    return jsonify(parse_response(data))
